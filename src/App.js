fe7a41f5181261ba5c8aff707019e1c6986ab9a2
Damian Epsztein <damiepsz3@gmail.com>
2018-08-13 18:12:27 -0300

    docs: :books: semantic ui added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import config from "./config"
@@ -9,42 +9,35 @@ class App extends Component {
     // 1. Load the JavaScript client library.
     window.gapi.load("client", () => {
       // 2. Initialize the JavaScript client library.
-      window.gapi.client
-        .init({
-          apiKey: config.apiKey,
-          // Your API key will be automatically added to the Discovery Document URLs.
-          discoveryDocs: config.discoveryDocs
-        })
-        .then(() => {
-          // 3. Initialize and make the API request.
-          this.load(this.onLoad);
-        });
+      window.gapi.client.init({
+        apiKey: config.apiKey,
+        // Your API key will be automatically added to the Discovery Document URLs.
+        discoveryDocs: config.discoveryDocs
+      }).then(() => {
+        // 3. Initialize and make the API request.
+        this.load(this.onLoad);
+      });
     });
   }
 
   load = (callback) => {
     window.gapi.client.load("sheets", "v4", () => {
-      window.gapi.client.sheets.spreadsheets.values
-        .get({
-          spreadsheetId: config.spreadsheetId,
-          range: "Sheet1!A1:A25"
-        })
-        .then(console.log);
+      window.gapi.client.sheets.spreadsheets.values.get({spreadsheetId: config.spreadsheetId, range: "Sheet1!A1:A25"}).then(console.log);
     });
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+    </div>);
   }
 }
 
