9084214bf4d30076cc597c878777172d385d6c31
Damian Epsztein <damiepsz3@gmail.com>
2018-08-13 17:39:01 -0300

    feat: :sparkles: fetching info from google sheet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import config from "./config"
 
 class App extends Component {
+  componentDidMount() {
+    console.log(process.env.API_KEY);
+    // 1. Load the JavaScript client library.
+    window.gapi.load("client", () => {
+      // 2. Initialize the JavaScript client library.
+      window.gapi.client
+        .init({
+          apiKey: config.apiKey,
+          // Your API key will be automatically added to the Discovery Document URLs.
+          discoveryDocs: config.discoveryDocs
+        })
+        .then(() => {
+          // 3. Initialize and make the API request.
+          this.load(this.onLoad);
+        });
+    });
+  }
+
+  load = (callback) => {
+    window.gapi.client.load("sheets", "v4", () => {
+      window.gapi.client.sheets.spreadsheets.values
+        .get({
+          spreadsheetId: config.spreadsheetId,
+          range: "Sheet1!A1:A25"
+        })
+        .then(console.log);
+    });
+  }
+
   render() {
     return (
       <div className="App">
