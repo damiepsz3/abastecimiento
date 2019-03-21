82b592241f8b1f3fb75fa2593ea4cfd318670daf
Damian Epsztein <damiepsz3@gmail.com>
2019-03-21 17:47:54 -0300

    from hooks to classes

--- src/Components/admin/Nav/Download.js
+++ src/Components/admin/Nav/Download.js
@@ -5,86 +5,50 @@ const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
-const dataSet1 = [
-  {
-    name: "Johson",
-    amount: 30000,
-    sex: "M",
-    is_married: true
-  },
-  {
-    name: "Monika",
-    amount: 355000,
-    sex: "F",
-    is_married: false
-  },
-  {
-    name: "John",
-    amount: 250000,
-    sex: "M",
-    is_married: false
-  },
-  {
-    name: "Josef",
-    amount: 450500,
-    sex: "M",
-    is_married: true
-  }
-];
-
-var dataSet2 = [
-  {
-    name: "Johnson",
-    total: 25,
-    remainig: 16
-  },
-  {
-    name: "Josef",
-    total: 25,
-    remainig: 7
-  }
-];
-
 export default class Download extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    console.log(this.props.solicitudes);
     return (
       <ExcelFile
         element={
-          <Button className="descargar" floated="right" icon="download" />
+          <Button
+            className='descargar'
+            floated='right'
+            icon='download'
+            onClick={() => this.props.deleteSol()}
+          />
         }
       >
-        <ExcelSheet data={this.props.solicitudes} name="Solicitudes">
+        <ExcelSheet data={this.props.solicitudes} name='Solicitudes'>
           <ExcelColumn
-            label="Material_a_solic"
+            label='Material_a_solic'
             value={col =>
               col.estado === "rechazada"
                 ? `Rechazada - ${col.razon}`
                 : col.numeroMaterial
             }
           />
-          <ExcelColumn label="Nombre_apellido" value="nombreApellido" />
-          <ExcelColumn label="Mail_solicitante" value="email" />
-          <ExcelColumn label="Fecha_pidio" value="createdDate" />
+          <ExcelColumn label='Nombre_apellido' value='nombreApellido' />
+          <ExcelColumn label='Mail_solicitante' value='email' />
+          <ExcelColumn label='Fecha_pidio' value='createdDate' />
 
           <ExcelColumn
-            label="Plantilla"
+            label='Plantilla'
             value={col => col.plantillaSeleccionada["Nombre Plantilla"]}
           />
           <ExcelColumn
-            label="Clas_n2_UNSPSC"
+            label='Clas_n2_UNSPSC'
             value={col => col.plantillaSeleccionada["Taxonomia BOLD"]}
           />
           <ExcelColumn
-            label="Caract_1"
+            label='Caract_1'
             value={col => col.plantillaSeleccionada["Característica 1"]}
           />
           <ExcelColumn
-            label="Val_caract_1"
+            label='Val_caract_1'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -94,11 +58,11 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Caract_2"
+            label='Caract_2'
             value={col => col.plantillaSeleccionada["Característica 2"]}
           />
           <ExcelColumn
-            label="Val_caract_2"
+            label='Val_caract_2'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -108,11 +72,11 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Caract_3"
+            label='Caract_3'
             value={col => col.plantillaSeleccionada["Característica 3"]}
           />
           <ExcelColumn
-            label="Val_caract_3"
+            label='Val_caract_3'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -122,11 +86,11 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Caract_4"
+            label='Caract_4'
             value={col => col.plantillaSeleccionada["Característica 4"]}
           />
           <ExcelColumn
-            label="Val_caract_4"
+            label='Val_caract_4'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -136,11 +100,11 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Caract_5"
+            label='Caract_5'
             value={col => col.plantillaSeleccionada["Característica 5"]}
           />
           <ExcelColumn
-            label="Val_caract_5"
+            label='Val_caract_5'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -150,11 +114,11 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Caract_6"
+            label='Caract_6'
             value={col => col.plantillaSeleccionada["Característica 6"]}
           />
           <ExcelColumn
-            label="Val_caract_6"
+            label='Val_caract_6'
             value={col =>
               !col.numeroMaterial
                 ? col.camposDinamicos[
@@ -164,36 +128,36 @@ export default class Download extends React.Component {
             }
           />
           <ExcelColumn
-            label="Unid_med_suj"
+            label='Unid_med_suj'
             value={col =>
               col.unidadMedida ? /\[([^)]+)\]/.exec(col.unidadMedida)[1] : ""
             }
           />
 
-          <ExcelColumn label="Present" value="presentacion" />
-          <ExcelColumn label="Homologado" value="" />
-          <ExcelColumn label="Planta_req" value="opcionPlanta" />
-          <ExcelColumn label="Sector_apr" value="opcionSector" />
+          <ExcelColumn label='Present' value='presentacion' />
+          <ExcelColumn label='Homologado' value='' />
+          <ExcelColumn label='Planta_req' value='opcionPlanta' />
+          <ExcelColumn label='Sector_apr' value='opcionSector' />
           <ExcelColumn
-            label="Criticidad"
+            label='Criticidad'
             value={col => col.criticidad.charAt(1)}
           />
           <ExcelColumn
-            label="Reparable"
+            label='Reparable'
             value={col => (col.repara ? "Si" : "No")}
           />
-          <ExcelColumn label="TAG_utiliza" value="valorTAG" />
-          <ExcelColumn label="Val_unit" value="valorUSD" />
+          <ExcelColumn label='TAG_utiliza' value='valorTAG' />
+          <ExcelColumn label='Val_unit' value='valorUSD' />
           <ExcelColumn
-            label="Req_stk"
+            label='Req_stk'
             value={col => (col.requiereStock ? "Si" : "No")}
           />
-          <ExcelColumn label="Cons_anual_est" value="consumoAnual" />
-          <ExcelColumn label="Tiempo_aprovisionamiento" value="" />
-          <ExcelColumn label="estado" value="estado" />
-          <ExcelColumn label="Texto breve de material" value="" />
-          <ExcelColumn label="Texto Datos Básicos" value="" />
-          <ExcelColumn label="Texto Ampliado" value="" />
+          <ExcelColumn label='Cons_anual_est' value='consumoAnual' />
+          <ExcelColumn label='Tiempo_aprovisionamiento' value='' />
+          <ExcelColumn label='estado' value='estado' />
+          <ExcelColumn label='Texto breve de material' value='' />
+          <ExcelColumn label='Texto Datos Básicos' value='' />
+          <ExcelColumn label='Texto Ampliado' value='' />
         </ExcelSheet>
       </ExcelFile>
     );
--- src/Components/admin/Nav/index.js
+++ src/Components/admin/Nav/index.js
@@ -1,71 +1,100 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Component, Fragment } from "react";
 import { withRouter, Link } from "react-router-dom";
 import { compose } from "recompose";
 import { Tab, Input, Button, Menu, Dropdown, Icon } from "semantic-ui-react";
 import Tarjetas from "../Tarjetas";
 import Download from "./Download";
 import "../Admin.css";
-
 import { withFirebase } from "../../../Firebase";
-//import { useCollection } from "react-firebase-hooks/firestore";
-import { useObjectVal } from "react-firebase-hooks/database";
 
-import { CSVLink } from "react-csv";
+class Nav extends Component {
+  state = {
+    solProc: [],
+    solPen: [],
+    search: "",
+    filter: "createdDate",
+    loading: true
+  };
 
-const Nav = ({ firebase, match, history }) => {
-  const [solProc, setSolProc] = useState([]);
-  const [solPen, setSolPen] = useState([]);
-  const [search, setSearch] = useState("");
-  const [download, setDownload] = useState([]);
-  const [filter, setFilter] = useState("createdDate");
-  const { error, loading, value } = useObjectVal(
-    firebase.db.ref("solicitudes")
-  );
+  componentDidMount() {
+    //llamada firebase
+    this.props.firebase.db.ref("solicitudes").on("value", snapshot => {
+      const value = snapshot.val();
+      if (value) {
+        const solicitudes = Object.keys(value).map(k => {
+          const { createdDate, ...rest } = value[k];
+          return { id: k, createdDate: new Date(createdDate), ...rest };
+        });
+        this.setState({
+          solPen: solicitudes
+            .filter(sol => sol.estado === "pendiente")
+            .sort(this.sortBy),
+          solProc: solicitudes
+            .filter(sol => sol.estado !== "pendiente")
+            .sort(this.sortBy),
+          loading: false
+        });
+      } else {
+        this.setState({ loading: false });
+      }
+    });
+  }
 
-  const sortBy = (a, b) => {
-    if (a[filter] > b[filter]) return -1;
-    if (a[filter] < b[filter]) return 1;
+  sortBy = (a, b) => {
+    if (a[this.state.filter] > b[this.state.filter]) return -1;
+    if (a[this.state.filter] < b[this.state.filter]) return 1;
     return 0;
   };
 
-  const panes = [
+  panes = [
     {
       menuItem: (
-        <Menu.Item key="1" as={Link} to="/admin/pendientes">
-          {loading ? "Pendientes (...)" : `Pendientes (${solPen.length})`}
+        <Menu.Item key='1' as={Link} to='/admin/pendientes'>
+          {this.state.loading
+            ? "Pendientes (...)"
+            : `Pendientes (${this.state.solPen.length})`}
         </Menu.Item>
       ),
       render: () => (
         <Tab.Pane attached={false}>
           <Tarjetas
-            loading={loading}
-            tarjetaSeleccionada={match.params.idSolicitud}
+            tarjetaSeleccionada={this.props.match.params.idSolicitud}
             solicitudes={
-              search.length > 0
-                ? solPen
+              this.state.search.length > 0
+                ? this.state.solPen
                     .filter(s => {
                       if (s.plantillaSeleccionada !== "") {
                         return (
-                          s.nombreApellido.toLowerCase().includes(search) ||
-                          s.opcionPlanta.toLowerCase().includes(search) ||
-                          s.id.toLowerCase().includes(search) ||
+                          s.nombreApellido
+                            .toLowerCase()
+                            .includes(this.state.search) ||
+                          s.opcionPlanta
+                            .toLowerCase()
+                            .includes(this.state.search) ||
+                          s.id.toLowerCase().includes(this.state.search) ||
                           s.plantillaSeleccionada["Nombre Plantilla"]
                             .toLowerCase()
-                            .includes(search) ||
+                            .includes(this.state.search) ||
                           s.plantillaSeleccionada["Taxonomia BOLD:Descripción"]
                             .toLowerCase()
-                            .includes(search)
+                            .includes(this.state.search)
                         );
                       }
                       return (
-                        s.nombreApellido.toLowerCase().includes(search) ||
-                        s.id.toLowerCase().includes(search) ||
-                        s.opcionPlanta.toLowerCase().includes(search) ||
-                        s.numeroMaterial.toLowerCase().includes(search)
+                        s.nombreApellido
+                          .toLowerCase()
+                          .includes(this.state.search) ||
+                        s.id.toLowerCase().includes(this.state.search) ||
+                        s.opcionPlanta
+                          .toLowerCase()
+                          .includes(this.state.search) ||
+                        s.numeroMaterial
+                          .toLowerCase()
+                          .includes(this.state.search)
                       );
                     })
-                    .sort(sortBy)
-                : solPen.sort(sortBy)
+                    .sort(this.sortBy)
+                : this.state.solPen.sort(this.sortBy)
             }
           />
         </Tab.Pane>
@@ -73,41 +102,53 @@ const Nav = ({ firebase, match, history }) => {
     },
     {
       menuItem: (
-        <Menu.Item key="2" as={Link} to="/admin/procesadas">
-          {loading ? "Procesadas (...)" : `Procesadas (${solProc.length})`}
+        <Menu.Item key='2' as={Link} to='/admin/procesadas'>
+          {this.state.loading
+            ? "Procesadas (...)"
+            : `Procesadas (${this.state.solProc.length})`}
         </Menu.Item>
       ),
       render: () => (
         <Tab.Pane attached={false}>
           <Tarjetas
-            tarjetaSeleccionada={match.params.idSolicitud}
-            loading={loading}
+            tarjetaSeleccionada={this.props.match.params.idSolicitud}
+            loading={this.state.loading}
             solicitudes={
-              search.length > 0
-                ? solProc
+              this.state.search.length > 0
+                ? this.state.solProc
                     .filter(s => {
                       if (s.plantillaSeleccionada !== "") {
                         return (
-                          s.nombreApellido.toLowerCase().includes(search) ||
-                          s.opcionPlanta.toLowerCase().includes(search) ||
-                          s.id.toLowerCase().includes(search) ||
+                          s.nombreApellido
+                            .toLowerCase()
+                            .includes(this.state.search) ||
+                          s.opcionPlanta
+                            .toLowerCase()
+                            .includes(this.state.search) ||
+                          s.id.toLowerCase().includes(this.state.search) ||
                           s.plantillaSeleccionada["Nombre Plantilla"]
                             .toLowerCase()
-                            .includes(search) ||
+                            .includes(this.state.search) ||
                           s.plantillaSeleccionada["Taxonomia BOLD:Descripción"]
                             .toLowerCase()
-                            .includes(search)
+                            .includes(this.state.search)
                         );
                       }
                       return (
-                        s.nombreApellido.toLowerCase().includes(search) ||
-                        s.id.toLowerCase().includes(search) ||
-                        s.opcionPlanta.toLowerCase().includes(search) ||
-                        s.numeroMaterial.toLowerCase().includes(search)
+                        s.nombreApellido
+                          .toLowerCase()
+                          .includes(this.state.search) ||
+                        s.id.toLowerCase().includes(this.state.search) ||
+                        s.opcionPlanta
+                          .toLowerCase()
+                          .includes(this.state.search) ||
+                        s.numeroMaterial
+                          .toLowerCase()
+                          .includes(this.state.search)
                       );
                     })
-                    .sort(sortBy)
-                : solProc.sort(sortBy)
+                    .sort(this.sortBy)
+                : this.state.solProc.sort(this.sortBy)
             }
           />
         </Tab.Pane>
@@ -115,168 +156,61 @@ const Nav = ({ firebase, match, history }) => {
     }
   ];
 
-  const filterOption = [
+  filterOption = [
     { text: "Fecha", value: "createdDate" },
     { text: "Planta", value: "opcionPlanta" },
     { text: "Estado", value: "estado" }
   ];
 
-  //esta funcion saca el valor de la unidad de medida entre corchete. ej: para Kilogramo [KG] devuelve KG
-  function obtenerValorEntreCorchetes(texto) {
-    var valorEntreCorchetes = /\[([^)]+)\]/.exec(texto);
-    if (valorEntreCorchetes) return valorEntreCorchetes[1];
-    return texto;
+  render() {
+    return (
+      <Fragment>
+        <div className='topNavBar'>
+          <Tab
+            menu={{ secondary: true, pointing: true }}
+            panes={this.panes}
+            defaultActiveIndex={
+              this.props.match.params.type === "procesadas" ? 1 : 0
+            }
+          />
+          <Download
+            solicitudes={this.state.solProc}
+            deleteSol={() =>
+              this.props.firebase.deleteSolicitudes(
+                this.state.solProc.map(s => s.id)
+              )
+            }
+          />
+          <Input
+            className='barraBusqueda'
+            icon={{ name: "search", link: true }}
+            placeholder='Search...'
+            onChange={(e, { value }) =>
+              this.setState({ search: value.toLowerCase() })
+            }
+          />
+          <Dropdown
+            className='filtrarPor'
+            placeholder='Ordenar por'
+            selection
+            options={
+              this.props.match.params.type === "procesadas"
+                ? this.filterOption
+                : this.filterOption.filter(opt => opt.text !== "Estado")
+            }
+            onChange={(e, { value }) => this.setState({ filter: value })}
+          />
+          <Button
+            className='cerrarSesion'
+            floated='right'
+            icon='sign out'
+            onClick={() => this.props.firebase.doSignOut()}
+          />
+        </div>
+      </Fragment>
+    );
   }
-
-  const descargar = () => {
-    const json = solProc.map(s => {
-      if (s.plantillaSeleccionada)
-        return {
-          Material_a_solic:
-            s.estado === "rechazada"
-              ? `Rechazada - ${s.razon}`
-              : s.numeroMaterial,
-          Nombre_apellido: s.nombreApellido,
-          Mail_solicitante: s.email,
-          Fecha_pidio: s.createdDate,
-          Plantilla: s.plantillaSeleccionada["Nombre Plantilla"],
-          Clas_n2_UNSPSC: s.plantillaSeleccionada["Taxonomia BOLD"],
-          Caract_1: s.plantillaSeleccionada["Característica 1"],
-          Val_caract_1:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 1"]],
-          Caract_2: s.plantillaSeleccionada["Característica 2"],
-          Val_caract_2:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 2"]],
-          Caract_3: s.plantillaSeleccionada["Característica 3"],
-          Val_caract_3:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 3"]],
-          Caract_4: s.plantillaSeleccionada["Característica 4"],
-          Val_caract_4:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 4"]],
-          Caract_5: s.plantillaSeleccionada["Característica 5"],
-          Val_caract_5:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 5"]],
-          Caract_6: s.plantillaSeleccionada["Característica 6"],
-          Val_caract_6:
-            s.camposDinamicos[s.plantillaSeleccionada["Característica 6"]],
-          Unid_med_suj: obtenerValorEntreCorchetes(s.unidadMedida),
-          Present: s.presentacion,
-          Homologado: null,
-          Planta_req: s.opcionPlanta,
-          Sector_apr: s.opcionSector,
-          Criticidad: s.criticidad.charAt(1),
-          Reparable: s.repara ? "Si" : "No",
-          TAG_utiliza: s.valorTAG,
-          Val_unit: s.valorUSD,
-          Req_stk: s.requiereStock ? "Si" : "No",
-          Cons_anual_est: s.requiereStock ? s.consumoAnual : null,
-          Tiempo_aprovisionamiento: null,
-          estado: s.estado,
-          "Texto breve de material": "",
-          "Texto Datos Básicos": "",
-          "Texto Ampliado": ""
-        };
-      return {
-        Material_a_solic:
-          s.estado === "rechazada"
-            ? `Rechazada - ${s.razon}`
-            : s.numeroMaterial,
-        Nombre_apellido: s.nombreApellido,
-        Mail_solicitante: s.email,
-        Fecha_pidio: s.createdDate,
-        Plantilla: "",
-        Clas_n2_UNSPSC: "",
-        Caract_1: "",
-        Val_caract_1: "",
-        Caract_2: "",
-        Val_caract_2: "",
-        Caract_3: "",
-        Val_caract_3: "",
-        Caract_4: "",
-        Val_caract_4: "",
-        Caract_5: "",
-        Val_caract_5: "",
-        Caract_6: "",
-        Val_caract_6: "",
-        Unid_med_suj: s.unidadMedida,
-        Present: s.presentacion,
-        Homologado: null,
-        Planta_req: s.opcionPlanta,
-        Sector_apr: s.opcionSector,
-        Criticidad: s.criticidad,
-        Reparable: s.repara ? "Si" : "No",
-        TAG_utiliza: s.valorTAG,
-        Val_unit: s.valorUSD,
-        Req_stk: s.requiereStock ? "Si" : "No",
-        Cons_anual_est: s.requiereStock ? s.consumoAnual : null,
-        Tiempo_aprovisionamiento: null,
-        estado: s.estado,
-        "Texto breve de material": "",
-        "Texto Datos Básicos": "",
-        "Texto Ampliado": ""
-      };
-    });
-    setDownload(json);
-    firebase.deleteSolicitudes(solProc.map(s => s.id));
-  };
-
-  useEffect(
-    () => {
-      if (!loading)
-        if (!error && value) {
-          const solicitudes = Object.keys(value).map(k => {
-            const { createdDate, ...rest } = value[k];
-            return { id: k, createdDate: new Date(createdDate), ...rest };
-          });
-
-          setSolPen(
-            solicitudes.filter(sol => sol.estado === "pendiente").sort(sortBy)
-          );
-          setSolProc(
-            solicitudes.filter(sol => sol.estado !== "pendiente").sort(sortBy)
-          );
-        }
-    },
-    [value]
-  );
-
-  return (
-    <Fragment>
-      <div className="topNavBar">
-        <Tab
-          menu={{ secondary: true, pointing: true }}
-          panes={panes}
-          defaultActiveIndex={match.params.type === "procesadas" ? 1 : 0}
-        />
-        <Download solicitudes={solProc} />
-        <Input
-          className="barraBusqueda"
-          icon={{ name: "search", link: true }}
-          placeholder="Search..."
-          onChange={(e, { value }) => setSearch(value.toLowerCase())}
-          value={search}
-        />
-        <Dropdown
-          className="filtrarPor"
-          placeholder="Ordenar por"
-          selection
-          options={
-            match.params.type === "procesadas"
-              ? filterOption
-              : filterOption.filter(opt => opt.text !== "Estado")
-          }
-          onChange={(e, { value }) => setFilter(value)}
-        />
-        <Button
-          className="cerrarSesion"
-          floated="right"
-          icon="sign out"
-          onClick={() => firebase.doSignOut()}
-        />
-      </div>
-    </Fragment>
-  );
-};
+}
 
 export default compose(
   withFirebase,
