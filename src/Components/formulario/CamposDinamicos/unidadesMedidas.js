bb0bec134c605f4e34b8d3b7891558ae18737166
Guiille <guillebril@gmail.com>
2019-05-09 17:49:48 +0200

    arregle error de"["

--- src/Components/formulario/CamposDinamicos/unidadesMedidas.js
+++ src/Components/formulario/CamposDinamicos/unidadesMedidas.js
@@ -1,84 +1,84 @@
 export default [
   {
     key: "1",
-    value: "Metro [M]",
-    text: "Metro [M]"
+    value: "Metro (M)",
+    text: "Metro (M)"
   },
 
   {
     key: "2",
-    value: "Metro cúbico [M3]",
-    text: "Metro cúbico [M3]"
+    value: "Metro cúbico (M3)",
+    text: "Metro cúbico (M3)"
   },
 
   {
     key: "3",
-    value: "Metro cuadrado [M2]",
-    text: "Metro cuadrado [M2]"
+    value: "Metro cuadrado (M2)",
+    text: "Metro cuadrado (M2)"
   },
 
   {
     key: "4",
-    value: "Kit [KIT]",
-    text: "Kit [KIT]"
+    value: "Kit (KIT)",
+    text: "Kit (KIT)"
   },
 
   {
     key: "5",
-    value: "Juego [JG]",
-    text: "Juego [JG]"
+    value: "Juego (JG)",
+    text: "Juego (JG)"
   },
 
   {
     key: "6",
-    value: "Tonelada [TO]",
-    text: "Tonelada [TO]"
+    value: "Tonelada (TO)",
+    text: "Tonelada (TO)"
   },
 
   {
     key: "7",
-    value: "Rollos [ROL]",
-    text: "Rollos [ROL]"
+    value: "Rollos (ROL)",
+    text: "Rollos (ROL)"
   },
 
   {
     key: "8",
-    value: "Bolsas [BOL]",
-    text: "Bolsas [BOL]"
+    value: "Bolsas (BOL)",
+    text: "Bolsas (BOL)"
   },
 
   {
     key: "9",
-    value: "Kilogramo [KG]",
-    text: "Kilogramo [KG]"
+    value: "Kilogramo (KG)",
+    text: "Kilogramo (KG)"
   },
 
   {
     key: "10",
-    value: "Pares [PAR]",
-    text: "Pares [PAR]"
+    value: "Pares (PAR)",
+    text: "Pares (PAR)"
   },
 
   {
     key: "11",
-    value: "Litros [L]",
-    text: "Litros [L]"
+    value: "Litros (L)",
+    text: "Litros (L)"
   },
 
   {
     key: "12",
-    value: "Talonarios [TAL]",
-    text: "Talonarios [TAL]"
+    value: "Talonarios (TAL)",
+    text: "Talonarios (TAL)"
   },
 
   {
     key: "13",
-    value: "Pieza [PZA]",
-    text: "Pieza [PZA]"
+    value: "Pieza (PZA)",
+    text: "Pieza (PZA)"
   },
   {
     key: "14",
-    value: "Unidad [UN]",
-    text: "Unidad [UN]"
+    value: "Unidad (UN)",
+    text: "Unidad (UN)"
   }
 ];
